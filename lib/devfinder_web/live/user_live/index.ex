8ecf7f6ad0cb4aa74e71d9c08d981bcc825be3b5
amos-kibet <kibetamos97@gmail.com>
2023-01-10 09:57:42 +0300

    refactor code

--- lib/devfinder_web/live/user_live/index.ex
+++ lib/devfinder_web/live/user_live/index.ex
@@ -8,22 +8,21 @@ defmodule DevfinderWeb.UserLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    # hardcoded = "amos-kibet"
-
     {:ok, assign(socket, :user_bio, %User{})}
-    |> IO.inspect(label: "[INDEX MOUNT SOCKET]")
   end
 
   @impl true
-  def handle_event("search", %{"user_bio" => %{"username" => username} = _user_params}, socket) do
+  def handle_event("search", %{"user_bio" => %{"username" => username}} = _user_params, socket) do
     case get_user(username) do
+      :finch_error ->
+        Logger.error("Something went wrong!")
+        {:noreply, socket}
       :not_found ->
         Logger.info("Username not found!")
         {:noreply, socket}
 
       user_bio ->
         {:noreply, assign(socket, :user_bio, user_bio)}
-        |> IO.inspect(label: "[HANDLE_EVENT SOCKET]")
     end
   end
 
@@ -32,14 +31,11 @@ defmodule DevfinderWeb.UserLive.Index do
       {:error, :finch_error} ->
         Logger.error("Something went wrong!")
         :finch_error
-
       {:error, :not_found} ->
         Logger.info("Username not found!")
         :not_found
-
       {:ok, user} ->
         user
-        |> IO.inspect(label: "[HANDLE_EVENT GET_USER USER]")
     end
   end
 end
