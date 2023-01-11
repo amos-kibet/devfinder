df6e49b34854a1e6d3f91cbdf1b675da49819e18
amos-kibet <kibetamos97@gmail.com>
2023-01-11 16:35:51 +0300

    retrieve user bi from gen_server

--- lib/devfinder_web/live/user_live/index.ex
+++ lib/devfinder_web/live/user_live/index.ex
@@ -7,35 +7,54 @@ defmodule DevfinderWeb.UserLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
+    send(self(), :retrieve_user_bio)
+
     {:ok, assign(socket, :user_bio, get_user_bio("octocat"))}
   end
 
-  @impl true
-  def handle_event("search", %{"user_bio" => %{"username" => username}} = _user_params, socket) do
-    case get_user_bio(username) do
-      :not_found ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Username not found! Try another")
-         |> assign(:user_bio, get_user_bio("octocat"))}
-
-      user_bio ->
-        {:noreply, assign(socket, :user_bio, user_bio)}
-    end
-  end
-
   def get_user_bio(username) do
-    case Core.get_user_bio(username) do
-      {:error, :finch_error} ->
-        Logger.error("Something went wrong! Retrying")
-        Core.get_user_bio(username)
-
-      {:error, :not_found} ->
-        Logger.info("Username not found! Try another")
-        :not_found
-
-      {:ok, user} ->
-        user
-    end
+    Core.retrieve_user_bio(username)
+    |> IO.inspect(label: "[USER_LIVE GET_USER_BIO]")
   end
+
+  @impl true
+  def handle_info(:retrieve_user_bio, socket) do
+    IO.inspect(socket, label: "[HANDLE_INFO SOCKET]")
+
+    user_bio = Core.retrieve_user_bio(socket.assigns.user_bio.username)
+    IO.inspect(user_bio, label: "[HANDLE_INFO USER_BIO]")
+
+    {:noreply, socket}
+  end
+
+  # @impl true
+  # def handle_event("search", %{"user_bio" => %{"username" => username}} = _user_params, socket) do
+  #   case get_user_bio(username) do
+  #     :not_found ->
+  #       {:noreply,
+  #        socket
+  #        |> put_flash(:info, "Username not found! Try another")
+  #        |> assign(:user_bio, get_user_bio("octocat"))}
+
+  #     user_bio ->
+  #       {:noreply, assign(socket, :user_bio, user_bio)}
+  #   end
+  # end
+
+  # def get_user_bio(username) do
+  #   case Core.get_user_bio(username) do
+  #     {:error, :finch_error} ->
+  #       Logger.error("Something went wrong! Retrying")
+
+  #       # FIXME: use recursion here as below
+  #       get_user_bio(username)
+
+  #     {:error, :not_found} ->
+  #       Logger.info("Username not found! Try another")
+  #       :not_found
+
+  #     {:ok, user} ->
+  #       user
+  #   end
+  # end
 end
