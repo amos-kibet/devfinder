1d96fc32fc34fe7ad07b0432b9b6892ed53be43a
amos-kibet <kibetamos97@gmail.com>
2023-01-13 16:15:04 +0300

    comment unused code

--- test/support/fixtures/core_fixtures.ex
+++ test/support/fixtures/core_fixtures.ex
@@ -1,14 +1,14 @@
-defmodule Devfinder.CoreFixtures do
-  @moduledoc """
-  This module defines test helpers for creating
-  entities via the `Devfinder.Core` context.
-  """
+# defmodule Devfinder.CoreFixtures do
+#   @moduledoc """
+#   This module defines test helpers for creating
+#   entities via the `Devfinder.Core` context.
+#   """
 
-  @doc """
-  Generate a user.
-  """
-  def user_fixture(username) do
-    {:ok, user} = Devfinder.Core.get_user_bio(username)
-    user
-  end
-end
+#   @doc """
+#   Generate a user.
+#   """
+#   def user_fixture(username) do
+#     {:ok, user} = Devfinder.Core.get_user_bio(username)
+#     user
+#   end
+# end
