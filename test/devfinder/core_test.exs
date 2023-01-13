affcd7d1c654a772d0a5942b286f4ae718480936
amos-kibet <kibetamos97@gmail.com>
2023-01-13 16:09:33 +0300

    comment unused code

--- test/devfinder/core_test.exs
+++ test/devfinder/core_test.exs
@@ -1,13 +1,13 @@
 defmodule Devfinder.CoreTest do
   use Devfinder.DataCase
 
-  @username "octocat"
+  # @username "octocat"
 
-  describe "users" do
-    import Devfinder.CoreFixtures
+  # describe "users" do
+  #   import Devfinder.CoreFixtures
 
-    test "get_user_bio/1 with valid data returns a user's GitHub profile" do
-      assert _user = user_fixture(@username)
-    end
-  end
+  #   test "get_user_bio/1 with valid data returns a user's GitHub profile" do
+  #     assert _user = user_fixture(@username)
+  #   end
+  # end
 end
