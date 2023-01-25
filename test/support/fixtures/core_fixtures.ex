1b5feda9fb91f4aa245beb4d698b5f72bda38dfa
amos-kibet <kibetamos97@gmail.com>
2023-01-25 13:19:11 +0300

    refactor tests

--- test/support/fixtures/core_fixtures.ex
+++ test/support/fixtures/core_fixtures.ex
@@ -1,14 +1,26 @@
 defmodule Devfinder.CoreFixtures do
-  @moduledoc """
-  This module defines test helpers for creating
-  entities via the `Devfinder.Core` context.
-  """
+  @moduledoc false
 
   @doc """
-  Generate a user.
+  Generate a test GitHub user profile.
   """
-  def user_fixture(username) do
-    {:ok, user} = Devfinder.Core.get_user_bio(username)
-    user
+  def user_fixture do
+    %{
+      full_name: "Amos Kibet",
+      avatar_url: "https://avatars.githubusercontent.com/u/50356453?v=4",
+      username: "amos-kibet",
+      profile_url: "https://github.com/amos-kibet",
+      bio: "Fullstack Elixir developer",
+      location: "Nairobi, Kenya",
+      twitter_username: "@amos_kibet",
+      company: "Amozone",
+      blog: "https://www.amozone.blog",
+      created_at: "7 May 2019",
+      profile_stats: %{
+        public_repos: "107",
+        followers: "32",
+        following: "156"
+      }
+    }
   end
 end
