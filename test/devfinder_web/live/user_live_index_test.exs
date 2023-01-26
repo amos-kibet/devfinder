e038887b35add0d297feb9d90853f5722e2612b8
amos-kibet <kibetamos97@gmail.com>
2023-01-26 17:22:44 +0300

    refactor tests

--- test/devfinder_web/live/user_live_index_test.exs
+++ test/devfinder_web/live/user_live_index_test.exs
@@ -1,17 +1,7 @@
 defmodule DevfinderWeb.UserLiveIndexTest do
   use DevfinderWeb.ConnCase, async: true
 
-  import Mox
   import Phoenix.LiveViewTest
-
-  setup :set_mox_from_context
-  setup :verify_on_exit!
-
-  def api_client, do: Application.get_env(:devfinder, :api_client)
-
-  def get_user_bio(username) do
-    api_client().get_user_bio(username)
-  end
 
   describe "Pre-connected & connected state" do
     test "pre-connected", %{conn: conn} do
