f01c6502900466da2e51273c8df517e3d48cd5a6
amos-kibet <kibetamos97@gmail.com>
2023-01-26 17:22:03 +0300

    refactor tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,6 +5,3 @@ Application.ensure_all_started(:mox)
 
 Mox.defmock(Devfinder.ApiClientMock, for: Devfinder.ApiClientBehaviour)
 Mox.defmock(Devfinder.HttpClientMock, for: Devfinder.HttpClientBehaviour)
-
-# Application.put_env(:devfinder, :api_client, Devfinder.ApiClientBehaviour)
-# Application.put_env(:devfinder, :http_client, Devfinder.HttpClientBehaviour)
